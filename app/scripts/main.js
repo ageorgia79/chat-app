ec51f023aee0b18c81622238cd5917ef088f79a0
Andy Georgia <andygeorgia@hotmail.com>
2014-05-29 18:35:51 -0400

    homework start

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+var textAppend = _.template($('.text-append').text());
+
+
+$.getJSON("http://tiny-pizza-server.herokuapp.com/collections/chat-messages").done(function(data){
+  renderTextAppend(data)
+});
+
+
+
+function renderTextAppend (alltext) {
+    var rendered = textAppend(user);
+    console.log(rendered);
+    $('.textappend').prepend(rendered);
+};
+
+
+
+
+
+
