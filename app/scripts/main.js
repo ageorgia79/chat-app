ee7f08068b052e8f24bdaf45e41ac6b97f461469
Andy Georgia <andygeorgia@hotmail.com>
2014-05-30 12:32:24 -0400

    it works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,59 @@
-var textAppend = _.template($('.text-append').text());
+'use strict';
+
+var tinyServer;
+
+var user = 'Andy G';
+
+var msgbox = _.template($('.chat-message').text());
 
 
-$.getJSON("http://tiny-pizza-server.herokuapp.com/collections/chat-messages").done(function(data){
-  renderTextAppend(data)
+
+function renderChat(data) {
+
+  $('.textappend').empty();
+
+  for (var i = 0; i < data.length - 1; i += 1) {
+
+    if (data[i].message) {
+
+      var rendered = msgbox(data[i]);
+
+      $('.textappend').append(rendered);
+    }
+  }
+}
+
+function loadChat () {
+
+  $.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(data) {
+
+    tinyServer = data;
+
+    renderChat(data);
+  });
+}
+
+function Message(user, message, time) {
+  this.user = user || '';
+  this.message = message || '';
+  this.time = time || '';
+}
+
+function refreshChat(info) {
+  $.post('http://tiny-pizza-server.herokuapp.com/collections/chat-messages', info);
+}
+
+$('.send').click(function() {
+
+  var textbox = $('.textbox').val();
+  $('.textbox').val('');
+
+  var time = Date.now();
+
+  var outgoing = new Message(user, message, time);
+
+  refreshChat(outgoing);
 });
 
-
-
-function renderTextAppend (alltext) {
-    var rendered = textAppend(user);
-    console.log(rendered);
-    $('.textappend').prepend(rendered);
-};
-
-
-
-
-
+setInterval(loadChat, 1000);
 
